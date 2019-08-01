cdc6c0b734f62e1f90c593b3cd697ba234fdebbe
Rami Awadallah <rami.moh.awadallah@gmail.com>
2019-08-01 16:42:24 +0300

    Update reset.blade.php

--- src/Resources/admin/admins/passwords/reset.blade.php
+++ src/Resources/admin/admins/passwords/reset.blade.php
@@ -1,47 +1,46 @@
 @extends('layouts.auth')
 @section('content')
-<div class="content">
-    <div class="row">
-        <div class="col-md-8">
-            <div class="block">
-                <div class="block-header">{{ ucfirst(config('multiauth.prefix')) }} Reset Password</div>
 
-                <div class="block-content">
+<!-- Page Content -->
+<div class="hero-static d-flex align-items-center">
+    <div class="w-100">
+        <!-- Sign In Section -->
+        <div class="content content-full bg-white">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-4 py-4">
+                    <!-- Header -->
+                    <div class="text-center mb-5">
+                        <p class="mb-2">
+                            <img  class="logo-main img-fluid text-primary" src="{{ theme('backend/media/favicons/logo.png') }}">
+                        </p>
+                        <h1 class="h4 mb-1">
+                            {{ ucfirst(config('multiauth.prefix')) }} Reset Password
+                        </h1>
+                    </div>
+
                     <form method="POST" action="{{ route('admin.password.request') }}" aria-label="{{ __('Admin Reset Password') }}">
                         @csrf
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}"
+                        <div class="form-group">
+                                <input id="email" type="email" class="form-control form-control-lg form-control-alt {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}"
                                     required autofocus> @if ($errors->has('email'))
-                                <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span> @endif
-                            </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
+                        <div class="form-group">
+                                <input id="password" type="password" class="form-control form-control-lg form-control-alt {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
                                     required> @if ($errors->has('password'))
                                 <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
-                                    </span> @endif
-                            </div>
+                                </span> @endif
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                        <div class="form-group">
+                                <input id="password-confirm" type="password" class="form-control form-control-lg form-control-alt" name="password_confirmation" required>
                         </div>
 
                         <div class="form-group row mb-0">
