094ddea5541fc8a702f98ba6d1e7b5e6676becf1
Rami Awadallah <rami.moh.awadallah@gmail.com>
2020-05-08 18:12:42 +0300

    delete old data

--- src/Resources/Helpers/bsForm/deleteSelect.blade.php
+++
@@ -1,7 +0,0 @@
-@php  $id = isset($id) ? $id : '';  @endphp
-
-<div class="custom-control custom-checkbox">
-    <input id="user_id_{{ $id }}" type="checkbox" value="{{ $id }}" class="custom-control-input checkSingle"
-           id="user_id_1" required name="item[]=">
-           <label class="custom-control-label" for="user_id_{{ $id }}"></label>
-</div>
--- src/Resources/admin/langs/show.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.backend')
-@section('title') {{ trans('lang.languages') }}  @endsection
-@section('content')
-
-
- 
-@endsection
