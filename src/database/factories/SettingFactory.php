3296b1c2947ce68f46c97353268f818a2546f652
Rami Awadallah <rami.moh.awadallah@gmail.com>
2022-01-22 04:45:52 +0400

    update

--- src/database/factories/SettingFactory.php
+++ src/database/factories/SettingFactory.php
@@ -1,38 +1,44 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
+namespace Database\Factories;
 
-use App\Models\Setting;
-use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-$factory->define(Setting::class, function (Faker $faker) {
-    return [
-        //
-        'title' => 'Ramivel',
-        'subtitle' => 'Ramivel CMS',
-        'email' => 'rami.moh.awadallah@gmail.com',
-        'phone' => null,
-        'address' => null,
-        'fax' => null,
-        'pobox' => '11118',
-        'map' => null,
-        'mainvideo' => null,
-        // About your website
-        'content' => null,
-        'logo' => null,
-        'icon' => null,
-        'maintenance' => null,
-        'keywords' => null,
-        'copyright' => null,
-        // Social media
-        'facebook' => null,
-        'twitter' => null,
-        'instagram' => null,
-        'linkedin' => null,
-        'youtube' => null,
-
-        'theme' => 'modern',
-
-        'updated_by' => 'admin'
-    ];
-});
+class SettingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'title' => 'Ramivel',
+            'subtitle' => 'Ramivel CMS',
+            'email' => 'rami.moh.awadallah@gmail.com',
+            'phone' => null,
+            'address' => null,
+            'fax' => null,
+            'pobox' => '11118',
+            'map' => null,
+            'mainvideo' => null,
+            // About your website
+            'content' => null,
+            'logo' => null,
+            'icon' => null,
+            'maintenance' => null,
+            'keywords' => null,
+            'copyright' => null,
+            // Social media
+            'facebook' => null,
+            'twitter' => null,
+            'instagram' => null,
+            'linkedin' => null,
+            'youtube' => null,
+            'theme' => 'modern',
+            'updated_by' => 'admin'
+        ];
+    }
+}
--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Setting;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        App\Models\Settin::factory(1)->create();
+        // \App\Models\User::factory(10)->create();
+        Setting::factory(1)->create();
     }
 }
